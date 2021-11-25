30c9d0fef98ae2ff119918a82effd80e96d5bb3c
Robs <robsonline@live.com>
2021-11-24 22:14:26 -0300

    ok

--- routers/Grunge.routes.js
+++ routers/Grunge.routes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router(); 
 const GrungeController = require("../controller/Grunge.controller"); 
 
-router.get("/listar",GrungeController.getAll);
+router.get("/listall",GrungeController.getAll);
 
-router.get("/listarnome/:id", GrungeController.getSingle);
+router.get("/listname/:id", GrungeController.getSingle);
 
-router.post('/adicionar', GrungeController.postCreate);
+router.post('/add', GrungeController.postCreate);
 
 router.put('/update/:id', GrungeController.putUpdate);
 
-router.delete("/deletar/:id", GrungeController.delDelete);
+router.delete("/delete/:id", GrungeController.delDelete);
 
 module.exports = router;
 
